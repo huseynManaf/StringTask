60415234ac15217a4a5f1cf7207770855f0e9bd0
huseynManaf <huseynim@code.edu.az>
2023-01-15 07:11:29 -0800

    init

---
+++ DifferenceTime/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+using System;
+
+namespace DifferenceTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DiiferenceTime.DiifferenceTime("09:00:00", "12:00:30", "23:15:30"));
+        }
+    }
+}
